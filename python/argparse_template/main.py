b693c0c125b637af095d10d1d60468ea7268656a
Penano <s6215054@palermo.local>
2018-06-18 19:16:51 -0400

    add python/ folder

---
+++ python/argparse_template/main.py
@@ -0,0 +1,57 @@
+# see https://gitlab.decisionsciences.bns/mosaic/label-generation
+# how to use argparse with a json type configuration file
+# usage: python main.py config.json
+
+import argparse
+import sys
+import json
+
+
+try:
+    # Python 3.
+    from json import JSONDecodeError
+except ImportError:
+    # Python 2 (ValueError is raised for JSON decoding problems).
+    JSONDecodeError = ValueError
+
+_PROGRAM_DESCRIPTION = \
+    """How to use argparse
+    """
+
+
+def parse_args():
+    """Parse and return the command-line arguments for the program.
+
+    Returns
+    -----------
+    dict
+        Arguments parsed from the command line.
+
+    """
+    parser = argparse.ArgumentParser(
+        description=_PROGRAM_DESCRIPTION,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("config_file", help="path to the configuration file")
+    return vars(parser.parse_args())
+
+
+def main(configuration):
+
+    print(configuration)
+
+    #############################################################
+    # Code that uses arguements stored in config.json goes here #
+    #############################################################
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    try:
+        with open(args['config_file']) as f:
+            config = json.load(f)
+    except IOError:
+        sys.exit(1)
+    except JSONDecodeError:
+        sys.exit(1)
+
+    main(config)
---
+++ python/python.md
@@ -0,0 +1,30 @@
+# Notes, templates, and techniques on the Python programming language
+
+
+## Template for argparse
+
+
+## Basic template for functions decorators
+```python
+def my_decorator(my_function):
+    def wrapper(x):
+        print("Test before function call")
+        my_function(x)
+        print("Test after function call")
+    return wrapper
+
+
+def my_function(x):
+    print("This is my_function with arguement {}".format(x))
+
+
+my_function = my_decorator(my_function)
+my_function("arg")
+
+# Using decorator syntactic sugar
+@my_decorator
+def my_function():
+    print("This is my_function!")
+
+my_function("arg")
+```
